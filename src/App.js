69f594fc61e27b39033c6c235b903f2fefe1c0d7
matijastampfel <matija.stampfel@gmail.com>
2017-05-26 21:00:21 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Button from './Button';
+import Form from './Form';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Form/>
+       <Button/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Form.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Form from './Form';
+
+describe('Test Form', ()=>{
+    it('Testing for forms',()=>{
+      let wrapper = shallow(<Form/>)
+      const input = <input type="text"/>; 
+      expect(wrapper.contains(input)).toBe(true);
+    })
+})
